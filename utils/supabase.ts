c9d3e83d8046322d8c1261f642e1f9d85d295ac1
Natalie Glavak <natalieglavak@Mac.lan>
2026-04-22 12:56:06 +0200

    ...

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-// Vi skapar en klient som hämtar nycklarna direkt från din .env.local fil
+// en klient som hämtar nycklarna direkt från din .env.local fil
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
